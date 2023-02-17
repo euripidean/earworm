2672a9da258f9c4242bcad7ea09559a7a556741e
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-02-17 14:44:01 -0800

    Added Flask Migrate

--- app/extensions.py
+++ app/extensions.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 from app.config import Config
 import os
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 ###########################
 # Authentication
