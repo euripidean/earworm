201205a85250ed38fb2446dc52493d126d4fc80d
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-02-22 19:24:21 -0800

    Setting Flask Migrate back up

--- earworm/extensions.py
+++ earworm/extensions.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 ###########################
 # Authentication
